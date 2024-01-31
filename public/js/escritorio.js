faa35d2288dd69a9d12803092d0164c8027cd64c
Genaro Salomone <genarosalomone@gmail.com>
2024-01-31 10:57:17 -0300

    Prepare Desk Screen

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,44 @@
 
+//Referencias HTML
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
 
-console.log('Escritorio HTML');
+
+
+const searchParams = new URLSearchParams(window.location.search);
+if (!searchParams.has('escritorio')) {
+  window.location = 'index.html';
+  throw new Error("Desk is required");
+}
+
+const desk = searchParams.get('escritorio');
+lblEscritorio.innerText = desk;
+
+console.log({ escritorio: desk });
+
+
+const socket = io();
+
+socket.on('connect', () => {
+  btnAtender.disabled = false;
+});
+
+socket.on('last-ticket', (last) => {
+  // text = 'Ticket ' + last;
+  // console.log(text);
+  // lblNewTicket.innerText = text;
+});
+
+
+socket.on('disconnect', () => {
+  btnAtender.disabled = true;
+});
+
+
+btnAtender.addEventListener('click', () => {
+
+  // socket.emit('next-ticket', null, (ticket) => {
+  //   lblNewTicket.innerText = ticket;
+  // });
+
+});
